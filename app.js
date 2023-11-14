ccdd3a7e9a4211b68f9ccaf7684ad199dce116c2
JonnyACCI <jshan9078@gmail.com>
2023-11-14 04:15:03 +0000

    Added profile image

--- app.js
+++ app.js
@@ -61,11 +61,11 @@ $('a[href*="#"]')
     }
   });
 
-window.onload = function()  //executes when the page finishes loading
-{
-  setTimeout(func1, 3000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
+// window.onload = function()  //executes when the page finishes loading
+// {
+//   setTimeout(func1, 3000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
 
-};
-function func1() {
-  document.getElementById("connect").className = "cta3";
-}
+// };
+// function func1() {
+//   document.getElementById("connect").className = "cta3";
+// }
